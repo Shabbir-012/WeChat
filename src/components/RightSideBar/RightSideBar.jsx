1adf4c6b93c208933bf1dbbd1a3b477d30d38fac
Shabbir-012 <shabbirahmedfaysal@gmail.com>
2025-04-28 12:02:44 +0600

    chat box completed

--- src/components/RightSideBar/RightSideBar.jsx
+++ src/components/RightSideBar/RightSideBar.jsx
@@ -1,17 +1,36 @@
-import React from 'react'
-import './RightSideBar.css'
-import assets from '../../assets/assets'
+import React from "react";
+import "./RightSideBar.css";
+import assets from "../../assets/assets";
 
 const RightSideBar = () => {
   return (
     <div className="rs">
       <div className="rs-profile">
         <img src={assets.profile_img} alt="" />
-        <h3>Richar Sanford <img src={assets.green_dot} className='dot' alt="" /></h3>
+        <h3>
+          Richar Sanford <img src={assets.green_dot} className="dot" alt="" />
+        </h3>
         <p>Hey, There i am Richard Sanford using chat app</p>
       </div>
-    </div>
-  )
-}
+      <hr />
 
-export default RightSideBar
+      <div className="rs-media">
+        <p>Media</p>
+        <div>
+          <img src={assets.pic1} alt="" />
+          <img src={assets.pic2} alt="" />
+          <img src={assets.pic3} alt="" />
+          <img src={assets.pic4} alt="" />
+          <img src={assets.pic1} alt="" />
+          <img src={assets.pic1} alt="" />
+        </div>
+      </div>
+
+      <div>
+        <button>Logout</button>
+      </div>
+    </div>
+  );
+};
+
+export default RightSideBar;
